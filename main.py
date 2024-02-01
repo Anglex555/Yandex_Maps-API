3da5bd0362b39235ae01bdab0add582ca76984b8
Anglex555 <87623102+Anglex555@users.noreply.github.com>
2024-02-01 18:16:27 +0300

    Commit 0.2
    Оконное приложение на PyQt5 для отображения карты с использованием API Яндекс.Карт.
    
    Основные особенности:
    - Возможность перемещения центра карты с помощью мыши.
    - Возможность изменения масштаба карты с помощью колесика мыши.
    - Смена режимов карты.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QComboBox
+from PyQt5.QtGui import QPixmap, QPainter, QCursor
+from PyQt5.QtCore import Qt, QPointF
 import requests
 
 
@@ -12,26 +12,52 @@ class MapApplication(QWidget):
         self.apikey = "Место_для_api"
         self.center = {'lon': 37.617635, 'lat': 55.755814}
         self.zoom = 10
+        self.map_type = 'map'
+        self.is_mouse_pressed = False
 
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(100, 100, 1200, 900)
+        self.setGeometry(100, 100, 1000, 520)
         self.setWindowTitle('Maps API')
 
         self.map_label = QLabel(self)
-        self.map_label.setGeometry(0, 0, 1200, 900)
+        self.map_label.setGeometry(0, 0, 780, 520)
 
+        self.map_type_combo = QComboBox(self)
+        self.map_type_combo.addItem("Схема")
+        self.map_type_combo.addItem("Спутник")
+        self.map_type_combo.addItem("Гибрид")
+        self.map_type_combo.setGeometry(740, 50, 150, 30)
+
+        self.map_type_combo.setStyleSheet("""
+            QComboBox {
+                font-size: 14px;
+                border: 2px solid #4A90E2;
+                border-radius: 5px;
+                padding: 3px;
+            }
+            QComboBox::drop-down {
+                subcontrol-origin: padding;
+                subcontrol-position: top right;
+                width: 20px;
+                border-left-width: 2px;
+                border-left-color: #4A90E2;
+                border-left-style: solid;
+            }
+        """)
+
+        self.map_type_combo.activated[str].connect(self.onMapTypeChange)
         self.update_map()
 
     def update_map(self):
         lon = self.center['lon']
         lat = self.center['lat']
-        
+
         map_params = {
             "ll": f"{lon},{lat}",
             "z": self.zoom,
-            "l": "map",
+            "l": self.map_type,
             "pt": f"{lon},{lat},pm2blm",
         }
 
@@ -42,8 +68,17 @@ class MapApplication(QWidget):
             f.write(response.content)
 
         pixmap = QPixmap('map.png')
-        pixmap = pixmap.scaled(1200, 800, Qt.KeepAspectRatio)
+        pixmap = pixmap.scaled(780, 520, Qt.KeepAspectRatio)
         self.map_label.setPixmap(pixmap)
+
+    def onMapTypeChange(self, text):
+        if text == "Схема":
+            self.map_type = 'map'
+        elif text == "Спутник":
+            self.map_type = 'sat'
+        elif text == "Гибрид":
+            self.map_type = 'sat,skl'
+        self.update_map()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Up:
@@ -63,11 +98,33 @@ class MapApplication(QWidget):
         delta = event.angleDelta().y() / 120
         if delta > 0:
             self.zoom += 1
+            print(self.zoom)
+            if self.zoom > 20:
+                self.zoom = 20
         else:
             self.zoom -= 1
-            if self.zoom < 0:
-                self.zoom = 0
+            if self.zoom < 1:
+                self.zoom = 1
         self.update_map()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.is_mouse_pressed = True
+            self.start_pos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.is_mouse_pressed:
+            delta = event.pos() - self.start_pos
+            lon_scale = 360 / (2 ** (self.zoom + 8))
+            lat_scale = 170 / (2 ** (self.zoom + 8))
+            self.center['lon'] -= delta.x() * lon_scale
+            self.center['lat'] += delta.y() * lat_scale
+            self.update_map()
+            self.start_pos = event.pos()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.is_mouse_pressed = False
 
 
 if __name__ == '__main__':
